1fabda249dd704638c28bd8153c9faab11e2752c
davadaviar <davadaviarsaputra2345@gmail.com>
2023-08-06 15:35:51 +0700

    Finish hero section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,9 @@ import { navbar } from '../constants'
 
 const Navbar = () => {
   return (
-    <nav className='navbar w-full flex justify-between items-center px-24 py-10'>
+    <nav className='navbar w-full flex justify-between items-center px-24 py-10 fixed top-0'>
         <h3 className='logo font-heading text-[40px]'>Digiskuy</h3>
-        <ul className='navlink flex flex-1 gap-5 justify-center'>
+        <ul className='navlink flex flex-1 gap-8 justify-center'>
             {
                 navbar.links.map((link) => (
                     <li className='text-[16px]'>{link}</li>
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -5,12 +5,12 @@ import { ornament } from '../assets/images'
 
 const Hero = () => {
   return (
-    <section className={`${styles.paddingX} mt-24`}>
-        <h1 className='font-heading text-[120px] text-center leading-heading'>{hero.heading}</h1>
+    <section className={`${styles.paddingX} absolute overflow-x-hidden min-h-screen`}>
+        <h1 className='font-heading text-[120px] mt-60 text-center leading-heading'>{hero.heading}</h1>
         <p className='text-center leading-body mt-12 w-3/4 mx-auto'>{hero.body}</p>
+        <img className='w-[350px] h-[350px] absolute -left-44 top-1/2 -z-10' src={ornament} alt='ornamnet' />
+        <img className='w-[350px] h-[350px] absolute overflow-clip -right-44 top-32 -z-10' src={ornament} alt='ornamnet' />
         <div className='bg-blur w-[570px] h-[570px] bg-primary rounded-full absolute top-36 left-[30%] -z-10 blur-template'></div>
-        <img className='w-[350px] h-[350px] absolute overflow-clip -left-44 top-1/2 -z-10' src={ornament} alt='ornamnet' />
-        <img className='w-[350px] h-[350px] absolute overflow-x-clip -right-44 top-32 -z-10' src={ornament} alt='ornamnet' />
     </section>
   )
 }
