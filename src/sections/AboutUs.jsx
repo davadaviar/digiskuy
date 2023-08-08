ff8682196a6a2eb63349d30264fa5f6e1d7255a4
davadaviar <davadaviarsaputra2345@gmail.com>
2023-08-08 15:50:37 +0700

    Finish video responsive

--- src/sections/AboutUs.jsx
+++ src/sections/AboutUs.jsx
@@ -5,14 +5,18 @@ import { play } from '../assets/icons'
 
 const AboutUs = () => {
   return (
-    <section className={`about-us -mt-56 px-24`} id='2'>
-        <div className='video-player w-full h-[640px] mx-auto relative'>
+    <section className='about-us -mt-96 md:-mt-56 px-6 md:px-24' id='2'>
+        <div className='video-player w-full h-[245px] md:h-[640px] mx-auto md:relative'>
             <img src={videoPlayer} alt='video-player' />
-            <img className='w-[100px] h-[100px] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 shadow' src={play} alt='play' />
+            <img 
+              className='w-[40px] h-[40px] md:w-[100px] md:h-[100px] relative md:absolute -top-1/2 md:top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 shadow' 
+              src={play} 
+              alt='play' 
+            />
         </div>
-        <div className={`about-us__body mt-24 flex items-center justify-between`}>
-            <h2 className='font-heading text-[64px]'>{aboutUs.heading}</h2>
-            <p className='text-[20px] w-2/3 text-justify'>{aboutUs.body}</p>
+        <div className={`about-us__body mt-12 md:mt-24 flex flex-col md:flex-row md:items-center gap-4 md:justify-between`}>
+            <h2 className='font-heading text-[24px] md:text-[64px]'>{aboutUs.heading}</h2>
+            <p className='text-[16px] md:text-[20px] w-full md:w-2/3 text-justify'>{aboutUs.body}</p>
         </div>
     </section>
   )
--- src/sections/OurStack.jsx
+++ src/sections/OurStack.jsx
@@ -4,13 +4,13 @@ import Tilt from 'react-parallax-tilt';
 
 const OurStack = () => {
   return (
-    <section className='our-stack flex flex-1 justify-between items-center gap-44 px-24 mt-24'>
-      <div className='stack-glass w-1/2'>
+    <section className='our-stack flex flex-1 md:justify-between md:items-center md:gap-44 px-6 md:px-24 md:mt-24'>
+      <div className='stack-glass w-full md:w-1/2'>
         <Tilt>
           <img src={techStack} alt='tech-stack' />
         </Tilt>
       </div>
-      <h2 className='font-heading text-[120px] leading-heading'>Our Stack</h2>
+      <h2 className='font-heading md:text-[120px] leading-heading'>Our Stack</h2>
       <div className='bg-blur w-[570px] h-[570px] bg-primary rounded-full absolute left-[30%] -z-10 blur-template'></div>
     </section>
   )
